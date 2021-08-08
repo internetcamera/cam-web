43e79538e6ea05b8da488626f4963308e11bbcde
shahruz <hello@shahr.uz>
2021-08-08 15:08:06 -0700

    dependency updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import MainLayout from '@app/layouts/MainLayout';
+import PlausibleProvider from 'next-plausible';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <PlausibleProvider domain="cam.internet.camera">
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </PlausibleProvider>
   );
 }
 
